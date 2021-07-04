ec786d13e5f3b275ce93a9dca17a0c2857764649
suey0128 <syui0128@gmail.com>
2021-07-03 18:18:58 -0700

    commit tree c0de3c536e1fea6d278ff73f9a28ac18e915a20c

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,47 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year 
+  FROM books
+  WHERE series_id = 1"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto 
+  FROM characters
+  ORDER BY LENGTH(motto) DESC
+  LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(*) 
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT(*)  DESC
+  LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name 
+  FROM authors
+  LEFT JOIN subgenres ON authors.id = subgenres.id 
+  "
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  LEFT JOIN characters ON series.author_id = characters.author_id 
+  GROUP BY characters.species
+  ORDER BY COUNT(*)  DESC
+  LIMIT 1"
+  
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(*)
+  FROM characters
+  LEFT JOIN character_books ON character_books.character_id = characters.id 
+  GROUP BY characters.name
+  ORDER BY COUNT(*)  DESC, characters.name ASC
+  "
 end
